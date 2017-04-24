1cdbaea67f45105b6409963a0e18ec9482d21ab4
Omkar Salpekar <omkarsalpekar@airbears2-10-142-144-52.airbears2.1918.berkeley.edu>
2017-04-24 05:20:13 -0700

    reverting to previously functioning spec

--- java-redis-example.js
+++ java-redis-example.js
@@ -1,36 +1,36 @@
-// var redis = require("github.com/quilt/redis");
+var redis = require("github.com/quilt/redis");
 
-// var javaApp = new Container(new Image("java-redis-image",
-//   "FROM nginx\n" +
-//   "RUN cd /web_root && git clone github.com/osalpekar/java-redis-webapp"
-// ))
+var javaApp = new Container(new Image("java-redis-image",
+  "FROM nginx\n" +
+  "RUN cd /web_root && git clone github.com/osalpekar/java-redis-webapp"
+))
 
-// var namespace = createDeployment({});
+var namespace = createDeployment({});
 
-// var rds = new redis.Redis(1, "AUTH_PASSWORD");
-// rds.exclusive();
-// var jAppService = new Service("jApp", [javaApp]);
+var rds = new redis.Redis(1, "AUTH_PASSWORD");
+rds.exclusive();
+var jAppService = new Service("jApp", [javaApp]);
 
-// var baseMachine = new Machine({
-//     provider: "Amazon",
-//     cpu: new Range(1),
-//     ram: new Range(2),
-//     sshKeys: githubKeys("osalpekar"),
-// });
-
-// namespace.deploy(baseMachine.asMaster());
-// namespace.deploy(baseMachine.asWorker().replicate(2));
-
-// namespace.deploy(rds);
-// namespace.deploy(jAppService);
-
-
-var javaApp = require("github.com/osalpekar/java-redis-spec");
-var deployment = createDeployment();
 var baseMachine = new Machine({
-    provider: "Amazon", 
+    provider: "Amazon",
+    cpu: new Range(1),
+    ram: new Range(2),
     sshKeys: githubKeys("osalpekar"),
 });
-deployment.deploy(baseMachine.asMaster());
-deployment.deploy(baseMachine.asWorker());
-javaApp.Deploy(deployment)
+
+namespace.deploy(baseMachine.asMaster());
+namespace.deploy(baseMachine.asWorker().replicate(2));
+
+namespace.deploy(rds);
+namespace.deploy(jAppService);
+
+
+// var javaApp = require("github.com/osalpekar/java-redis-spec");
+// var deployment = createDeployment();
+// var baseMachine = new Machine({
+//     provider: "Amazon", 
+//     sshKeys: githubKeys("osalpekar"),
+// });
+// deployment.deploy(baseMachine.asMaster());
+// deployment.deploy(baseMachine.asWorker());
+// javaApp.Deploy(deployment)
