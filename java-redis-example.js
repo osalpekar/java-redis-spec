76076736b2b3fb018265a3e9eb375542dc3a8eb3
Omkar Salpekar <omkarsalpekar@airbears2-10-142-144-52.airbears2.1918.berkeley.edu>
2017-04-24 15:37:41 -0700

    pulling javaApp image from Docker Hub

--- java-redis-example.js
+++ java-redis-example.js
@@ -1,15 +1,33 @@
 var redis = require("github.com/quilt/redis");
 
-var javaApp = new Container(new Image("java-redis-image",
-  "FROM nginx\n" +
-  "RUN cd /web_root && git clone github.com/osalpekar/java-redis-webapp"
-))
+// var javaApp = new Container(new Image("java-redis-image",
+//   "FROM nginx\n" +
+//   "RUN cd /web_root && git clone github.com/osalpekar/java-redis-webapp"
+// ))
+
+var javaApp = new Container("osalpekar/test-nginx");
 
 var namespace = createDeployment({});
 
 var rds = new redis.Redis(1, "AUTH_PASSWORD");
 rds.exclusive();
 var jAppService = new Service("jApp", [javaApp]);
+
+
+publicInternet.connect(80, jAppService);
+jAppService.connect(80, publicInternet);
+jAppService.connect(443, publicInternet);
+jAppService.connect(53, publicInternet);
+jAppService.connect(8080, publicInternet);
+jAppService.connect(8081, publicInternet);
+jAppService.connect(4040, publicInternet);
+
+publicInternet.connect(8080, jAppService);
+publicInternet.connect(8081, jAppService);
+publicInternet.connect(80, jAppService);
+publicInternet.connect(53, jAppService);
+publicInternet.connect(443, jAppService);
+publicInternet.connect(4040, jAppService);
 
 var baseMachine = new Machine({
     provider: "Amazon",
@@ -19,7 +37,7 @@ var baseMachine = new Machine({
 });
 
 namespace.deploy(baseMachine.asMaster());
-namespace.deploy(baseMachine.asWorker().replicate(2));
+namespace.deploy(baseMachine.asWorker().replicate(4));
 
 namespace.deploy(rds);
 namespace.deploy(jAppService);
